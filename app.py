74f5b85415672f0e1ca90391dd30beda9715e1b8
Saurabh <61317144+0xSaurabhx@users.noreply.github.com>
2024-04-26 16:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 from pytube import YouTube
 from pydub import AudioSegment
+import shutil
 import os
+
+BOT_TOKEN = '6556103654:AAGDzRHqFPY7RCrAirbbNXNTO96EJI3yPic'
+CHANNEL_ID = '-1002144289063'
 
 st.set_page_config(page_icon="⬇️", page_title="Youtube MP3 & MP4 Downloader")
 
+password = st.sidebar.text_input("Enter Password", type='password')
+if password == "123":
+    if st.sidebar.button("Delete Files"):
+        if os.path.exists("./MP3"):
+            shutil.rmtree("./MP3")
+        if os.path.exists("./MP4"):
+            shutil.rmtree("./MP4")
+        st.success("Files deleted successfully!")
+else:
+    st.sidebar.write("Wrong Password")
+
 def main():
-    st.title("YouTube Video ⬇️loader")
+    st.title("YouTube Video Downloader")
     video_url = st.text_input("Enter YouTube Video URL:")
     format = st.selectbox("Select Format", ["MP4", "MP3"])
     if format == "MP4":
@@ -17,6 +32,7 @@ def main():
         if video_url:
             try:
                 yt = YouTube(video_url)
+                id = video_url.split("=")[-1]
                 if quality == "480p":
                     stream = yt.streams.filter(res="480p").first()
                 elif quality == "720p":
@@ -25,17 +41,30 @@ def main():
                     stream = yt.streams.filter(res="1080p").first()
                 if stream is None:
                     stream = yt.streams.get_highest_resolution()
-                
-                st.markdown(f"Downloading **{yt.title}** at {quality}...")
                 if format == "MP4":
-                    stream.download()
+                    with st.spinner("Downloading..."):
+                        stream.download(filename=f"{id}.mp4",output_path="./MP4/")
+                    with open(f"./MP4/{id}.mp4", "rb") as file:
+                        btn = st.download_button(
+                            label="Download File",
+                            data=file.read(),
+                            file_name=f"{id}.mp4",
+                        )
+                    st.success("MP4 Download completed successfully!")
                 elif format == "MP3":
-                    stream.download(filename="temp_video.mp4")
-                    video_path = "temp_video.mp4"
-                    audio_path = "temp_audio.mp3"
-                    AudioSegment.from_file(video_path).export(audio_path, format="mp3")
-                    os.remove(video_path)
-                    st.success("MP3 Download completed successfully!")
+                    with st.spinner("Downloading..."):
+                        stream.download(filename=f"{id}.mp4",output_path="./MP3/")
+                        video_path = f"./MP3/{id}.mp4"
+                        audio_path = f"./MP3/{id}.mp3"
+                    with st.spinner("Converting..."):
+                        AudioSegment.from_file(video_path).export(audio_path, format="mp3")
+                    with open(f"./MP3/{id}.mp4", "rb") as file:
+                        btn = st.download_button(
+                            label="Download File",
+                            data=file.read(),
+                            file_name=f"{id}.mp4",
+                        )
+                    st.success("MP3 Converted successfully!")
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
         else:
